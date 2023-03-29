4926e125065d2af0c6173ea60506d7ce3731c437
Kevin Rouwkema <kevinrouwkema@gmail.com>
2023-03-29 12:34:00 +0200

    first part of the first implementation

--- exercism/Records/Bandwagoner.elm
+++ exercism/Records/Bandwagoner.elm
@@ -1,22 +1,33 @@
 module Bandwagoner exposing (..)
 
 -- TODO: please define a type alias for the 'Coach' record
+type alias Coach = 
+    {
+        name : String
+        , formerPlayer : Bool
+    }
 -- TODO: please define the 'Stats' record
--- TODO: please define the 'Team' record
+Stats : { wins: Int, losses: Int}
 
+-- TODO: please define the 'Team' record
+Team : { name: String, coach: Coach, stats: Stats}
 
 replaceCoach newCoach team =
-    Debug.todo "Please implement this function"
+    {team | coach = newCoach}
 
-
+createTeam : String -> Stats -> Coach -> Team
 createTeam name stats coach =
-    Debug.todo "Please implement this function"
+    {
+        name = name
+        , stats = stats
+        , coach = coach
+    }
 
 
 
 -- TODO: use an extensible record to accept any record that has a `stats: Stats` field
 -- TODO: use pattern matching in the function parameter to get the `stats` field from the record
 
-
+rootForTeam : {a | stats : Stats}
 rootForTeam team =
     Debug.todo "Please implement this function, try to pattern match in the function parameter"
