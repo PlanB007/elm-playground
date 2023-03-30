6dd8fd24a734ef3bda0bbbf4643d474d4cf3e53d
Kevin Rouwkema <kevinrouwkema@gmail.com>
2023-03-30 12:18:12 +0200

    first attempt

--- exercism/Maybe/RolePlayingGame.elm
+++ exercism/Maybe/RolePlayingGame.elm
@@ -11,14 +11,34 @@ type alias Player =
 
 introduce : Player -> String
 introduce { name } =
-    Debug.todo "Please implement this function"
+    case name of
+        Nothing -> "Mighty Magician"
+        Just someName -> someName
 
 
 revive : Player -> Maybe Player
 revive player =
-    Debug.todo "Please implement this function"
+    let
+        isDead = player.health == 0
+        isDeadAndHighLevel = isDead && player.level >= 10
+    in
+        case player of 
+            isDeadAndHighLevel -> {player | health = 100, mana = 100}
+            isDead -> {player | health = 100, mana = Nothing}
+            not isDead -> Nothing
+        
 
 
 castSpell : Int -> Player -> ( Player, Int )
 castSpell manaCost player =
-    Debug.todo "Please implement this function"
+    let
+        successfulSpell = manaCost =< player.mana
+        damage = manaCost * 2
+        noMana = player.mana == Nothing || player.mana == 0
+        remainingMana = player.mana - manaCost
+        insufficientMana = manaCost > player.mana && player.mana > 0 
+    in
+        case player of
+            succesfulSpell -> ({player | mana = remainingMana }, damage)
+            noMana -> ({player | health = player.health - manaCost}, Nothing)
+            insufficientMana -> Nothing
